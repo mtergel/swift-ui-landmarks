a0769edfb60d73707b5e5a85583302a5f6f9a1c3
Tergel Munkhdelger <tergelm@gmail.com>
2023-02-03 15:43:14 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tergel Munkhdelger on 2023.02.03.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
