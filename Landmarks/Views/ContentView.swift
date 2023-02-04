3b9ed35d85289b37e62ebd656c1aea0897bc6e9d
Tergel Munkhdelger <tergelm@gmail.com>
2023-02-04 08:23:27 +0800

    Building Lists And Navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Tergel Munkhdelger on 2023.02.03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
